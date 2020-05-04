1d4ad23afb84502918573adcc6efd8a161319ff4
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-05-04 18:52:21 -0300

    criação do consign

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+const db = require("./config/db");
+const consign = require("consign");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Meu Backend!");
-});
+consign()
+  .then("./config/middlewares.js")
+  .then("./api")
+  .then("./config/routes.js")
+  .into(app);
+
+app.db = db;
 
 app.listen(3000, () => {
   console.log("Backend executando...");
