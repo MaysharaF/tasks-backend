af4bb602e6dd52bf053db46af054697845532c5f
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-05-11 16:45:42 -0300

    adicionando metodos para trativa das tarefas

--- api/task.js
+++ api/task.js
@@ -44,4 +44,31 @@ module.exports = (app) => {
       })
       .catch((err) => res.status(400).json(err));
   };
+
+  const updateTaskDoneAt = (req, res, doneAt) => {
+    app
+      .db("tasks")
+      .where({ id: req.params.id, userId: req.user.id })
+      .update({ doneAt })
+      .then((_) => res.status(204).send())
+      .catch((err) => res.status(400).json(err));
+  };
+
+  const toggleTask = (req, res) => {
+    app
+      .bd("tasks")
+      .where({ id: req.params.id, userId: req.user.id })
+      .first()
+      .then((task) => {
+        if (!task) {
+          const msg = `Task com id ${req.params.id} não encontrada.`;
+          return res.status(400).send(msg);
+        }
+
+        const doneAt = task.doneAt ? null : new Date();
+        updateTaskDoneAt(req, res, doneAt);
+      })
+      .catch((err) => res.status(400).json(err));
+  };
+  return { getTasks, save, remove, toggleTask };
 };
