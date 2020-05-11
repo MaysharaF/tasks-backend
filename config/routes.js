4477719bcf02b79754b3cddea636a0274c44ab3a
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-05-11 17:36:48 -0300

    correção de rota signin

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
   app.post("/signup", app.api.user.save);
-  app.post("/signin", app.api.auth.siggnin);
+  app.post("/signin", app.api.auth.signin);
 
   app
     .route("/tasks")
